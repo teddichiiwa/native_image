3d84a66e2ba4ab09c728a79fba5e8e543d0e22cd
Teddichiiwa <ptanhvm.ios@gmail.com>
2022-10-20 09:54:59 +0700

    feat: Implement cropImage function on Android

--- native_image/android/src/main/kotlin/com/teddichiiwa/native_image/NativeImagePlugin.kt
+++ native_image/android/src/main/kotlin/com/teddichiiwa/native_image/NativeImagePlugin.kt
@@ -1,5 +1,8 @@
 package com.teddichiiwa.native_image
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.util.Log
 import androidx.annotation.NonNull
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
@@ -7,6 +10,9 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.nio.ByteBuffer
 
 /** NativeImagePlugin */
 class NativeImagePlugin: FlutterPlugin, MethodCallHandler {
@@ -22,10 +28,57 @@ class NativeImagePlugin: FlutterPlugin, MethodCallHandler {
   }
 
   override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    if (call.method == "cropImage") {
+      val contextImage = BitmapFactory.decodeByteArray(
+        call.argument("bytes"),
+        0,
+        call.argument("length")!!
+      )
+      val cropWidth: Double = call.argument("width") ?: 0.0
+      val cropHeight: Double = call.argument("height") ?: 0.0
+
+      var cropX = 0.0
+      var cropY = 0.0
+      val cropRatio: Double = cropWidth / cropHeight
+      val originalRatio: Double = contextImage.width.toDouble() / contextImage.height.toDouble()
+      var scaledCropHeight = 0.0
+      var scaledCropWidth = 0.0
+
+      // See what size is longer and set crop rect parameters
+      if (originalRatio > cropRatio) {
+        scaledCropHeight = contextImage.height.toDouble()
+        scaledCropWidth = (contextImage.height / cropHeight) * cropWidth
+        cropX = (contextImage.width - scaledCropWidth) / 2
+        cropY = 0.0
+      } else {
+        scaledCropWidth = contextImage.width.toDouble()
+        scaledCropHeight = (contextImage.width / cropWidth) * cropHeight
+        cropY = (contextImage.height - scaledCropHeight) / 2
+        cropX = 0.0
+      }
+
+      val imageRef = Bitmap.createBitmap(contextImage, cropX.toInt(), cropY.toInt(), scaledCropWidth.toInt(), scaledCropHeight.toInt())
+      val croppedImageData = convertBitmapToByteArray(imageRef)
+      result.success(croppedImageData)
     } else {
       result.notImplemented()
+    }
+  }
+
+  private fun convertBitmapToByteArray(bitmap: Bitmap): ByteArray? {
+    var byteArrayOutputStream: ByteArrayOutputStream? = null
+    return try {
+      byteArrayOutputStream = ByteArrayOutputStream()
+      bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream)
+      byteArrayOutputStream.toByteArray()
+    } finally {
+      if (byteArrayOutputStream != null) {
+        try {
+          byteArrayOutputStream.close()
+        } catch (e: IOException) {
+          Log.e("NativeImagePlugin", "ByteArrayOutputStream was not closed")
+        }
+      }
     }
   }
 
